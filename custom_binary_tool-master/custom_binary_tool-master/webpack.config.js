a9e7c903ffeed49a4c9081e1df6dec7156ad12f3
Agbo140 <frankfx22@gmail.com>
2026-01-08 16:50:10 +0000

    Fix: Add @babel/preset-react for JSX support and update devServer config
    - Added @babel/preset-react to Babel presets to enable JSX syntax parsing
    - Updated devServer configuration to use localhost:3000 instead of localbot.binary.sx:80
    - This fixes the 'Support for the experimental syntax jsx' error

--- custom_binary_tool-master/custom_binary_tool-master/webpack.config.js
+++ custom_binary_tool-master/custom_binary_tool-master/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env'],
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
                     },
                 },
             },
@@ -78,9 +78,9 @@ module.exports = {
         }),
     ],
     devServer: {
-        port: 80,
-        host: 'localbot.binary.sx',
-        open: true,
+        port: 3000,
+        host: 'localhost',
+        open: false,
         historyApiFallback: true,
         client: {
             logging: 'log',
